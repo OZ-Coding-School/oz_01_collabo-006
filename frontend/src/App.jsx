e134056e8ed3eb2d84286fa2ec945d4e0998d135
sonsodockje <hanaroo0115@naver.com>
2024-04-04 18:55:39 +0900

    Setting 라우팅

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootPage from "./pages/RootPage";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootPage />,
-  },
-  {
-    path: "/detail/:id",
+    children: [
+      { path: "/", element: <MainPage /> },
+      { path: "/detail", element: <DetailPage /> },
+    ],
   },
 ]);
 
--- frontend/src/pages/RootPage.jsx
+++ frontend/src/pages/RootPage.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function RootPage() {
-  return <nav>네브바</nav>;
+  return (
+    <>
+      <nav>네브바</nav>
+      <Outlet />
+    </>
+  );
 }
 
 export default RootPage;
