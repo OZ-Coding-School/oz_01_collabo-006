aac80b6d67531ee8abe823fc69219e28e4afeccb
sonsodockje <hanaroo0115@naver.com>
2024-04-17 19:28:30 +0900

    Style app.js 에 mui 설정 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { Container } from '@mui/material'
 
 import _RootLayout from './components/layout/RootLayout'
 import DetailPage from './pages/DetailPage'
 import ErrorPage from './pages/ErrorPage'
 import MainPage from './pages/MainPage'
+
+const theme = createTheme({
+    palette: {
+        common: {
+            customWhite: '#ffffff',
+            customYellow: '#ffc145',
+            customRed: '#ff6b6c',
+            customPurple: '#b8b8d1',
+            customBlack: '#48495c',
+            customGray: '#a7a7a7',
+        },
+    },
+})
 
 const router = createBrowserRouter([
     {
@@ -18,7 +33,22 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-    return <RouterProvider router={router} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Container
+                maxWidth="md"
+                sx={{
+                    fontSize: {
+                        xs: theme.spacing(1.5), // 모바일
+                        md: theme.spacing(1.7), // 태블릿
+                        lg: theme.spacing(2), // 데스크톱
+                    },
+                }}
+            >
+                <RouterProvider router={router} />
+            </Container>
+        </ThemeProvider>
+    )
 }
 
 export default App
