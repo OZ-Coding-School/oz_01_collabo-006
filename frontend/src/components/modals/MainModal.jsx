9a9a1f51c766f5b7349a4fe178597e6a47b54d08
강계령 <kearung.kang@gmail.com>
2024-04-19 11:27:41 +0900

    Update 라디오버튼, 셀렉트박스 연결
    메인모달 라디오버튼과 필터네브바 셀렉트박스 연결. 맞는진모르겠지만 이그노어에 env파일 추가

--- frontend/src/components/modals/MainModal.jsx
+++ frontend/src/components/modals/MainModal.jsx
@@ -1,36 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { FiX } from 'react-icons/fi'
+import useStore from '../store' // store import 추가
+import './MainModal.css'
 import dogsData from '/images/dogs.jsx?url'
 import logo from '/images/logo.png'
-import './MainModal.css'
 
-const MainModal = () => {
-    const [open, setOpen] = useState(false)
+const MainModal = ({ isOpen, onClose }) => {
     const modalRef = useRef()
-    const [selectedDog, setSelectedDog] = useState(null)
+    const { selectedDog, setSelectedDog } = useStore() // 상태 및 액션 가져오기
 
-    // 페이지가 처음 로드될 때 모달 열기
     useEffect(() => {
-        setOpen(true)
-    }, [])
-
-    // 외부 클릭 시 모달 닫기 -> 사용자가 잘못 클릭 할 수 있으니 뺄까요
-    // const handleOutsideClick = (event) => {
-    //     if (modalRef.current && !modalRef.current.contains(event.target)) {
-    //         setOpen(false)
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     document.addEventListener('mousedown', handleOutsideClick)
-    //     return () => {
-    //         document.removeEventListener('mousedown', handleOutsideClick)
-    //     }
-    // }, [])
-
-    //모달 띄워져있는 상태에서 스크롤 막기
-    useEffect(() => {
-        if (open) {
+        if (isOpen) {
             document.body.style.overflow = 'hidden'
         } else {
             document.body.style.overflow = 'auto'
@@ -38,26 +18,22 @@ const MainModal = () => {
         return () => {
             document.body.style.overflow = 'auto'
         }
-    }, [open])
+    }, [isOpen])
 
-    const handleClose = () => setOpen(false)
-
-    // 선택된 개를 설정하는 함수
-    const handleDogSelect = (dog) => {
-        setSelectedDog(dog)
+    const handleRadioSelect = (event) => {
+        const selectedDogId = parseInt(event.target.value)
+        const selectedDog = dogsData.find((dog) => dog.id === selectedDogId)
+        setSelectedDog(selectedDog) // 액션 호출
     }
 
     const isButtonDisabled = selectedDog === null
 
     return (
         <>
-            {open && (
+            {isOpen && (
                 <div className="main-modal-background">
                     <div className="main-modal" ref={modalRef}>
-                        <button
-                            className="main-modal-exit"
-                            onClick={handleClose}
-                        >
+                        <button className="main-modal-exit" onClick={onClose}>
                             <FiX />
                         </button>
 
@@ -73,8 +49,8 @@ const MainModal = () => {
                                         className="main-modal-radio"
                                         type="radio"
                                         name="dogType"
-                                        value={dog.image}
-                                        onChange={() => handleDogSelect(dog)}
+                                        value={dog.id}
+                                        onChange={handleRadioSelect}
                                     />
                                     <div className="dog-component">
                                         <img
@@ -100,7 +76,7 @@ const MainModal = () => {
                         </div>
                         <button
                             className="main-modal-btn"
-                            onClick={handleClose}
+                            onClick={onClose}
                             disabled={isButtonDisabled}
                         >
                             페뜨하기
@@ -113,3 +89,119 @@ const MainModal = () => {
 }
 
 export default MainModal
+
+// import React, { useEffect, useRef, useState } from 'react'
+// import { FiX } from 'react-icons/fi'
+// import './MainModal.css'
+// import dogsData from '/images/dogs.jsx?url'
+// import logo from '/images/logo.png'
+
+// const MainModal = () => {
+//     const [open, setOpen] = useState(false)
+//     const modalRef = useRef()
+//     const [selectedDog, setSelectedDog] = useState(null)
+
+//     // 페이지가 처음 로드될 때 모달 열기
+//     useEffect(() => {
+//         setOpen(true)
+//     }, [])
+
+//     // 외부 클릭 시 모달 닫기 -> 사용자가 잘못 클릭 할 수 있으니 뺄까요
+//     // const handleOutsideClick = (event) => {
+//     //     if (modalRef.current && !modalRef.current.contains(event.target)) {
+//     //         setOpen(false)
+//     //     }
+//     // }
+
+//     // useEffect(() => {
+//     //     document.addEventListener('mousedown', handleOutsideClick)
+//     //     return () => {
+//     //         document.removeEventListener('mousedown', handleOutsideClick)
+//     //     }
+//     // }, [])
+
+//     //모달 띄워져있는 상태에서 스크롤 막기
+//     useEffect(() => {
+//         if (open) {
+//             document.body.style.overflow = 'hidden'
+//         } else {
+//             document.body.style.overflow = 'auto'
+//         }
+//         return () => {
+//             document.body.style.overflow = 'auto'
+//         }
+//     }, [open])
+
+//     const handleClose = () => setOpen(false)
+
+//     // 선택된 개를 설정하는 함수
+//     const handleDogSelect = (dog) => {
+//         setSelectedDog(dog)
+//     }
+
+//     const isButtonDisabled = selectedDog === null
+
+//     return (
+//         <>
+//             {open && (
+//                 <div className="main-modal-background">
+//                     <div className="main-modal" ref={modalRef}>
+//                         <button
+//                             className="main-modal-exit"
+//                             onClick={handleClose}
+//                         >
+//                             <FiX />
+//                         </button>
+
+//                         <img className="main-modal-logo" src={logo} />
+//                         <p className="main-modal-title">
+//                             내 반려동물을 사전 설정하고, 동반 가능 맞춤 정보를
+//                             제공받으세요!
+//                         </p>
+//                         <div className="dog-wrap">
+//                             {dogsData.map((dog) => (
+//                                 <label key={dog.id}>
+//                                     <input
+//                                         className="main-modal-radio"
+//                                         type="radio"
+//                                         name="dogType"
+//                                         value={dog.image}
+//                                         onChange={() => handleDogSelect(dog)}
+//                                     />
+//                                     <div className="dog-component">
+//                                         <img
+//                                             className="dog-image"
+//                                             src={
+//                                                 selectedDog === dog
+//                                                     ? dog.hoverImage
+//                                                     : dog.image
+//                                             }
+//                                             alt={dog.name}
+//                                         />
+//                                         <div className="dog-type-wrap">
+//                                             <p className="dog-title">
+//                                                 {dog.title}
+//                                             </p>
+//                                             <p className="dog-detail">
+//                                                 {dog.detail}
+//                                             </p>
+//                                         </div>
+//                                     </div>
+//                                 </label>
+//                             ))}
+//                         </div>
+//                         <button
+//                             className="main-modal-btn"
+//                             onClick={handleClose}
+//                             disabled={isButtonDisabled}
+//                         >
+//                             페뜨하기
+//                         </button>
+//                     </div>
+//                 </div>
+//             )}
+//         </>
+//     )
+// }
+
+// export default MainModal
