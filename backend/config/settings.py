e7215224efab32b2121d5fab98aa6ad05fe13cc5
JMHman <joenmh0710@gmail.com>
2024-04-26 01:33:24 +0900

    52머용 그는 신이야

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    # "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
