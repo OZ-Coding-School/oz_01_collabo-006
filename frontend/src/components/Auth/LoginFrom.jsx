48a06dd38d8744b155b4a1094ceb21b42499f224
rnjsdydwp9 <rnjsdydwp9@gmail.com>
2024-04-28 20:46:35 +0900

    Test-용

--- frontend/src/components/Auth/LoginFrom.jsx
+++ frontend/src/components/Auth/LoginFrom.jsx
@@ -1,68 +1,42 @@
-import { Link, useNavigate } from 'react-router-dom'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import { useState } from 'react'
-import IconButton from '@mui/material/IconButton'
-import InputAdornment from '@mui/material/InputAdornment'
-import Visibility from '@mui/icons-material/Visibility'
-import VisibilityOff from '@mui/icons-material/VisibilityOff'
-import OutlinedInput from '@mui/material/OutlinedInput'
-import InputLabel from '@mui/material/InputLabel'
-import FormControl from '@mui/material/FormControl'
-import axios from 'axios'
-import useLoginStore from '../../store/login'
+import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import axios from 'axios';
+
 function LoginFrom({ isLogin }) {
-    const setIsLogined = useLoginStore((state) => state.setIsLogined)
-    const isLogined = useLoginStore((state) => state.isLogined)
-    function naverHandler(e) {
-        e.preventDefault()
-        alert('hi')
-    }
-    const [showPassword, setShowPassword] = useState(false)
-    const handleClickShowPassword = () => setShowPassword((show) => !show)
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault()
-    }
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const navigate = useNavigate()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleNaverLogin = () => {
+        window.location.href = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=vpjcoqrRYMVgVTpRWfms&http://localhost:5173/auth?mode=login';
+    };
+
     const handleSubmit = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
         try {
             const response = await axios.post(
                 'http://175.45.192.12/api/v1/users/login/',
                 { email, password }
-            )
-            document.cookie = `access=${response.data.access};`
-            document.cookie = `refresh=${response.data.refresh};`
-            console.log('로그인 됨')
-            setIsLogined()
-            navigate(-1)
+            );
+            document.cookie = `access=${response.data.access};`;
+            document.cookie = `refresh=${response.data.refresh};`;
+            console.log('로그인 됨');
+            // 로그인 성공 후 처리
         } catch (error) {
-            console.error('로그인 실패:', error)
-            alert('아이디와 비밀번호를 다시 확인해주세요.')
+            console.error('로그인 실패:', error);
+            alert('아이디와 비밀번호를 다시 확인해주세요.');
         }
-    }
+    };
+
     return (
         <Box sx={{ mt: 7 }}>
-            <Typography
-                variant="h4"
-                gutterBottom
-                // sx={{ textAlign: 'center' }}
-            >
+            <Typography variant="h4" gutterBottom>
                 로그인
             </Typography>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: 2,
-                    mt: 2,
-                    mb: 2,
-                }}
-            >
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 2, mb: 2 }}>
                 <TextField
                     fullWidth
                     id="outlined-basic"
@@ -73,71 +47,46 @@ function LoginFrom({ isLogin }) {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
-                <FormControl variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password">
-                        비밀번호
-                    </InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-password"
-                        type={showPassword ? 'text' : 'password'}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                >
-                                    {showPassword ? (
-                                        <VisibilityOff />
-                                    ) : (
-                                        <Visibility />
-                                    )}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                        label="비밀번호"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </FormControl>
-                <Box></Box>
+                <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="비밀번호"
+                    name="password"
+                    variant="outlined"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <Button
                     variant="contained"
                     disableElevation
                     type="submit"
                     size="large"
-                    style={{
-                        backgroundColor: '#FFC145',
-                        height: '50px',
-                    }}
+                    style={{ backgroundColor: '#FFC145', height: '50px' }}
                     onClick={handleSubmit}
                 >
                     로그인
                 </Button>
-                <Button sx={{ p: 0 }} onClick={naverHandler}>
+                <Button
+                    sx={{ p: 0 }}
+                    onClick={handleNaverLogin}
+                >
                     <img
-                        src="/images/btnG_완성형.png"
-                        alt=""
-                        style={{
-                            width: '200px',
-                        }}
+                        src="/images/btnG_완성형.png" // 네이버 소셜 로그인 버튼 이미지 경로
+                        alt="네이버 로그인"
+                        style={{ width: '200px' }}
                     />
                 </Button>
             </Box>
             <div>
                 <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
-                    <Typography
-                        variant="span"
-                        gutterBottom
-                        sx={{ float: 'right' }}
-                    >
+                    <Typography variant="span" gutterBottom sx={{ float: 'right' }}>
                         회원이 아니신가요?
                     </Typography>
                 </Link>
             </div>
         </Box>
-    )
+    );
 }
-export default LoginFrom
+
+export default LoginFrom;
