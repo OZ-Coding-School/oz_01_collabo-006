39b5dfd9178f9d743f5edb8223ccbfeeebced92e
JMHman <joenmh0710@gmail.com>
2024-04-29 02:32:37 +0900

    견종검색 완료

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -249,12 +249,12 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "https://localhost:8080",
-    "http://0.0.0.0:8000",
-    "https://0.0.0.0:8000",
-    "http://223.130.139.240:8000",
-    "https://223.130.139.240:8000",
+    "http://localhost:80",
+    "https://localhost:80",
+    "http://0.0.0.0:80",
+    "https://0.0.0.0:80",
+    "http://223.130.153.84:80",
+    "https://223.130.153.84:80",
 ]
 # # 소셜 계정 설정
 # SOCIALACCOUNT_PROVIDERS = {
