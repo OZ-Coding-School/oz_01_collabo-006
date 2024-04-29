7ef970dd79928ada233422951af932de14ce198b
JMHman <joenmh0710@gmail.com>
2024-04-29 21:13:54 +0900

    섬네일 이미지 링크 완료

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -181,12 +181,13 @@ NCP_ACCESS_KEY = env('NCP_ACCESS_KEY')
 NCP_SECRET_KEY = env('NCP_SECRET_KEY')
 NCP_REGION_NAME = env('NCP_REGION_NAME')
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = NCP_BUCKET_NAME
 AWS_ACCESS_KEY_ID = NCP_ACCESS_KEY
 AWS_SECRET_ACCESS_KEY = NCP_SECRET_KEY
 AWS_S3_REGION_NAME = NCP_REGION_NAME
-AWS_S3_CUSTOM_DOMAIN = f'https://kr.object.ncloudstorage.com/{NCP_BUCKET_NAME}'
+AWS_S3_ENDPOINT_URL = 'https://kr.object.ncloudstorage.com'
+# AWS_S3_CUSTOM_DOMAIN = f'https://kr.object.ncloudstorage.com/{NCP_BUCKET_NAME}'
 
 
 
