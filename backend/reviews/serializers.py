4388ba6f278733f43d2e3086adda1214de91e4e9
JMHman <joenmh0710@gmail.com>
2024-04-30 04:48:41 +0900

    혜민 요구 사항

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -1,13 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Review
 
 class ReviewSerializer(ModelSerializer):
+  last_name = SerializerMethodField()  # SerializerMethodField를 사용하여 사용자의 이름 필드 추가
   class Meta:
     model = Review
     fields = (
       'id',
       'user',
+      'last_name',
       'place',
       'content',
       'created_at',
     )
+  
+  def get_user_name(self, obj):
+    return obj.user.last_name  # 사용자의 이름을 반환하는 메서드 작성
